92cc6c7556548f09913eae196c3c9d061d56975e
Andy Tam <andytam321@gmail.com>
2025-05-25 18:20:40 +0100

    feature: implement disabling submit button right after submit

--- static/reply.js
+++ static/reply.js
@@ -2,6 +2,8 @@ $("#f-reply").submit(e => {
     e.preventDefault()
     var formData = new FormData(e.target)
 
+    disableSubmit()
+
     fetch("/reply",{
         method: "POST",
         body: formData
@@ -9,14 +11,30 @@ $("#f-reply").submit(e => {
         response => response.json()
     ).then(
         json => {
-            if(json.message === "success"){
+            if (json.message === "success") {
                 location.href = "/"
             }
             else{
                 console.log(json)
                 window.alert("Failed. Something went wrong.")
+                enableSubmit()
             }
         }
     )
 
 })
+
+const disableSubmit = () => {
+    $("#submit-btn").text("Submitting...")
+    $("#submit-btn").prop("disabled", true)
+    $("#submit-btn").removeClass("btn-primary")
+    $("#submit-btn").addClass("btn-secondary")
+}
+
+const enableSubmit = () => {
+    $("#submit-btn").text("Submit")
+    $("#submit-btn").prop("disabled", false)
+    $("#submit-btn").removeClass("btn-secondary")
+    $("#submit-btn").addClass("btn-primary")
+}
+
